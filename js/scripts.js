c01da78451df6d75a6c5cfae6c87bc2bc08786e3
Adrian Zavala <zavaladrian@gmail.com>
2023-06-12 17:10:41 -0500

    6/12 project

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,24 @@ $(function () {
         }
     });
 });
+
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
+
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
